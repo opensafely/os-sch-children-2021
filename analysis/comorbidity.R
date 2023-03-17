d2332c869cacda192ab0358f43af66c72f3c420c
Tom Cowling <93713761+tecowling@users.noreply.github.com>
2023-03-17 12:16:25 +0000

    Edit gitignore to include R files

---
+++ analysis/comorbidity.R
@@ -0,0 +1,35 @@
+
+# load dataset
+df <- read.csv("output/input_comorbidity.csv")
+
+# set up blank matrix for results
+results <- matrix(nrow=12, ncol=4)
+
+# add conditions as row names
+conditions <- colnames(df)[1:nrow(results)]
+conditions <- gsub("_gp", "", conditions)
+conditions <- gsub("_", " ", conditions)
+conditions <- gsub("mi", "Myocardial infarction", conditions)
+conditions <- gsub("hf", "Heart failure ", conditions)
+rownames(results) <- stringr::str_to_sentence(conditions)
+
+# set column names
+colnames(results) <- c('Number of patients with record in TPP or SUS',
+                       '% of patients with record in TPP only',
+                       '% of patients with record in TPP and SUS',
+                       '% of patients with record in SUS only')
+
+# add values to results table
+for (i in 1:nrow(results)) {
+  results[i,1] <- sum(df[,i] == 1 | df[,i+nrow(results)] == 1)
+  results[i,2] <- sum(df[,i] == 1 & df[,i+nrow(results)] == 0)/results[i,1]
+  results[i,3] <- sum(df[,i] == 1 & df[,i+nrow(results)] == 1)/results[i,1]
+  results[i,4] <- sum(df[,i] == 0 & df[,i+nrow(results)] == 1)/results[i,1]
+}
+
+# format percentages
+library(scales)
+results[,2:4] <- apply(results[,2:4], 2, percent, accuracy = 0.1)
+
+#save table
+write.csv(results, file = "output/comorbidity_table.csv") 
